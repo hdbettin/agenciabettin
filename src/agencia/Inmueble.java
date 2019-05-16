18cef80035fd4dd92c8a3107fb13ef2a60136a89
MichaelNossa09 <MichaelNossaB@gmail.com>
2019-05-15 19:59:39 -0500

    agenciabettin

---
+++ src/agencia/Inmueble.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agencia;
+
+/**
+ *
+ * @author Estudiante
+ */
+public abstract class Inmueble {
+    protected String ubicacion;
+    protected double metrosc;
+    private double precio;
+
+    public Inmueble(String ubicacion, double metrosc) {
+        this.ubicacion = ubicacion;
+        this.metrosc = metrosc;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public double getMetrosc() {
+        return metrosc;
+    }
+    
+    public void muestra(){
+        System.out.println("ubicacion: " + this.ubicacion + " metrosc: " + this.metrosc);
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+    
+    
+    
+    
+}
